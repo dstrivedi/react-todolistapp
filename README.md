e8038e90c5dbf9e10718b5b0e097a5befe910b8f
dstrivedi <drashti1118@gmail.com>
2020-09-30 19:46:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-todolistapp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-todolistapp)
---
+++ src/Task/Task.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './Task.css'
+
+class Task extends Component {
+    state = {
+        tasks : ["Reading","Office Work","Hitting gym"],
+        value: ""
+    }
+
+    handlerChange = (event) => {
+        this.setState({value : event.target.value});
+    }
+
+    addTask = () => {
+        var newState = this.state.tasks;
+        newState.push(this.state.value);
+        this.setState({tasks: newState});
+        this.setState({value: ""});
+    }
+
+    deleteTask = (t) => {
+        var newState = this.state.tasks;
+        newState.pop(t);
+        this.setState({tasks: newState});
+    }
+
+    render() {
+        return (
+             <div className="task">
+                <ul className="taskName">
+                    {this.state.tasks.map(t => { 
+                        return (<li key={t} onClick={this.deleteTask.bind(this, t)}>{t}</li>)
+                    })}
+                </ul>
+                <input type="text" placeholder="Enter task" id="task" value={this.state.value} onChange={this.handlerChange} />
+                <input name="submit" type="submit" onClick={this.addTask} />
+             </div>
+        );
+    }
+}
+
+export default Task;
